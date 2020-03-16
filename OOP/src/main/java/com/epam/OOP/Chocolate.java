221ff22b94db0a00b125e74dee781c5236781685
Rohin <17pa1a1205@vishnu.edu.in>
2020-03-16 18:56:18 +0530

    First Commit

---
+++ OOP/src/main/java/com/epam/OOP/Chocolate.java
@@ -0,0 +1,11 @@
+package com.epam.OOP;
+public class Chocolate extends Sweets{
+    //inheritance
+    String name;
+    String type;
+    int cost;
+    int weigth;
+    Chocolate(String name,String type,int cost,int weigth){
+        super(name,type,cost,weigth);
+    }
+}
